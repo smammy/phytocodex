9117cd436e9da54fe8db524374acea7563b79828
Sam Birch <sam@porcupine.club>
2024-11-03 17:09:30 -0500

    crawler: make list parsing more robust

--- src/phytocodex/util/pathextractor.py
+++ src/phytocodex/util/pathextractor.py
@@ -9,22 +9,15 @@ def main():
     any_failure = False
 
     while path := sys.stdin.readline().rstrip("\r\n"):
-        with open(path, mode="rb") as file:
+        with open(path, errors="replace") as file:
             try:
                 soup = BeautifulSoup(file, "lxml")
                 for item in soup.find_all("div", {"class": "game-preview"}):
                     print(item.h2.a["href"])
             except Exception as ex:
                 any_failure = True
-                print(
-                    f"\nProblem making the soup!\nPath: {path}\n\n",
-                    *traceback.format_exception(ex),
-                    sep="",
-                    end="",
-                    file=sys.stderr,
-                )
-                continue
-
+                print(f"{path}: Problem making the soup!", file=sys.stderr)
+                traceback.print_exception(ex, file=sys.stderr)
 
     sys.exit(1 if any_failure else 0)
 
