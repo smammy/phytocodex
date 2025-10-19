c3488adea2d733c7900885388c9b7e3284cc9369
Sam Birch <sam@porcupine.club>
2025-10-19 14:09:31 -0400

    use a PGURL that will work in more cases

--- src/phytocodex/config.py
+++ src/phytocodex/config.py
@@ -4,6 +4,6 @@ HOST = os.environ.get("PHYT_HOST", "phytocodex.porcupine.club")
 PORT = os.environ.get("PHYT_PORT", "70")
 
 MGURL = os.environ.get("PHYT_MGURL", "http://macintoshgarden.org/")
-PGURL = os.environ.get("PHYT_PGURL", "postgresql://:5433/phytocodex")
+PGURL = os.environ.get("PHYT_PGURL", "postgresql://:5432/phytocodex")
 
 LOGLEVEL = os.environ.get("PHYT_LOGLEVEL", "INFO")
